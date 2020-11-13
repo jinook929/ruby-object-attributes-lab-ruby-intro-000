33cfbad2d61f6fa1a98395789a7e00fca1916731
JINOOK JUNG <jinook929@gmail.com>
2020-11-13 02:06:35 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Dog 
+  
+  def initialize(name, breed)
+    @name = name 
+    @breed = breed
+  end 
+  
+  def name
+    @name
+  end 
+    
+  def name=(this_name)
+    @name = this_name
+  end 
+  
+  def breed
+    @breed
+  end 
+  
+  def breed=(this_breed)
+    @breed = this_breed
+  end 
+end 
